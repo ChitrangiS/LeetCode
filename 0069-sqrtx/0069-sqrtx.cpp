b21bad2808d457ee18777e6bbd8c995380562def
ChitrangiS <165680976+ChitrangiS@users.noreply.github.com>
2026-07-10 07:43:13 +0530

    Time: 15 ms (9.93%), Space: 8.7 MB (14.09%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans=0;
+        for(int i=1;i<=x;i++){
+            if((long long)i*i<=x){
+                ans=i;
+            }else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
