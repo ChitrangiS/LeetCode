1327f35ed28e5324ead332e5ac21c936a121ba75
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-07-19 14:26:55 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.14%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+        if (n <= 0)
+            return false;
+
+        long long power = 1;
+
+        while (power < n) {
+            power *= 2;
+        }
+
+        return power == n;
+    }
+};
