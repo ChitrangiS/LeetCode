642ca9ed00430890feb70d70dfd74d8a60c8a9f8
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-07-21 19:46:47 +0530

    Time: 15 ms (69.85%), Space: 26.1 MB (38.21%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-    bool isAnagram(string a, string b) {
-        if (a.size() != b.size())
-            return false;
-
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
-        return a == b;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        vector<bool> vis(n, false);
-        vector<vector<string>> ans;
+        
+        //key:sorted ver,val:org
+        unordered_map<string,vector<string>>mp;
 
-        for (int i = 0; i < n; i++) {
-            if (vis[i])
-                continue;
-
-            vector<string> group;
-            group.push_back(strs[i]);
-            vis[i] = true;
-
-            for (int j = i + 1; j < n; j++) {
-
-                if (!vis[j] && isAnagram(strs[i], strs[j])) {
-
-                    group.push_back(strs[j]);
-                    vis[j] = true;
-                }
-            }
-
-            ans.push_back(group);
+        for(string s:strs){
+            string temp=s;
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(s);
         }
-
-        return ans;
+        vector<vector<string>>ans;
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+    return ans;
     }
 };
