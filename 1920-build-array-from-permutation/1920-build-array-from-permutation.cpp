16eb1f16b95e5a97512a7c8279c2a08ea0c86284
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-07-23 12:19:28 +0530

    Time: 0 ms (100%), Space: 20.6 MB (42.63%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+
+    void solve(vector<int>& nums, vector<int>& ans, int i) {
+
+        if (i == nums.size())
+            return;
+
+        ans[i] = nums[nums[i]];
+
+        solve(nums, ans, i + 1);
+    }
+
     vector<int> buildArray(vector<int>& nums) {
 
-        int n = nums.size();
+        vector<int> ans(nums.size());
 
-        vector<int> ans(n);
-
-        for(int i = 0; i < n; i++)
-        {
-            ans[i] = nums[nums[i]];
-        }
+        solve(nums, ans, 0);
 
         return ans;
     }
