bfbec84130926d05a40c40f6129e5cbadf156303
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-07-27 11:57:32 +0530

    Time: 0 ms (100%), Space: 10.7 MB (5.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i = 0;
-        int j = s.size() - 1;
-        while (i < j) {
-            while (i < j && !isalnum(s[i])) {
-                i++;
-            }
-            while (i < j && !isalnum(s[j])) {
-                j--;
-            }
-            if (tolower(s[i]) != tolower(s[j]))
-                return false;
-            i++;
-            j--;
+        string temp;
+        for (char ch : s) {
+            if (isalnum(ch))
+                temp += tolower(ch);
         }
-        return true;
+        string rev = temp;
+        reverse(temp.begin(), temp.end());
+
+        return temp == rev;
     }
 };
-// O(N),O(1)
