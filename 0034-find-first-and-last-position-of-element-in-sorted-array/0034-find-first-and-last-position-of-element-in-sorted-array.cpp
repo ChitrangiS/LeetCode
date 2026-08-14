9d8dc0f774578467f878f17cfd193da48fb7f8ab
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-08-14 10:11:26 +0530

    Time: 0 ms (100%), Space: 17.5 MB (54.41%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,49 +1,49 @@
 class Solution {
 public:
-    int firstOccur(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0;
-        int h = n - 1;
-        int ans = -1;
-
-        while (l <= h) {
-            int mid = (l + h) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                h = mid - 1;
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
-            }
-        }
-        return ans;
-    }
-    int lastOccur(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0;
-        int h = n - 1;
-        int ans = -1;
-
-        while (l <= h) {
-            int mid = (l + h) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                l = mid + 1; // Search right
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
-            }
-        }
-
-        return ans;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = firstOccur(nums, target);
-        int last = lastOccur(nums, target);
+
+        int n = nums.size();
+
+        int first = -1;
+        int last = -1;
+
+        int l = 0;
+        int r = n - 1;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) {
+                first = mid;
+                r = mid - 1;       
+            }
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        l = 0;
+        r = n - 1;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                l = mid + 1;       
+            }
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
 
         return {first, last};
     }
