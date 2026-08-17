cc47eaee636cc654e598f0eec9fd7570f4991eaa
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-08-17 21:59:27 +0530

    Time: 0 ms (100%), Space: 9.2 MB (81.4%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
         vector<int> freq(26, 0);
-    vector<bool> used(26, false);
+        vector<bool> used(26, false);
 
-    for(char ch : s) {
-        freq[ch - 'a']++;
-    }
-
-    string ans;
-
-    for(char ch : s) {
-
-        freq[ch - 'a']--;
-
-        if(used[ch - 'a'])
-            continue;
-
-        while(!ans.empty() &&
-              ans.back() > ch &&
-              freq[ans.back() - 'a'] > 0) {
-
-            used[ans.back() - 'a'] = false;
-            ans.pop_back();
+        for (char ch : s) {
+            freq[ch - 'a']++;
         }
 
-        ans.push_back(ch);
-        used[ch - 'a'] = true;
-    }
+        string ans;
 
-    return ans;
+        for (char ch : s) {
 
+            freq[ch - 'a']--;
+
+            if (used[ch - 'a'])
+                continue;
+
+            while (!ans.empty() && ans.back() > ch &&
+                   freq[ans.back() - 'a'] > 0) {
+
+                used[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(ch);
+            used[ch - 'a'] = true;
+        }
+
+        return ans;
     }
 };
