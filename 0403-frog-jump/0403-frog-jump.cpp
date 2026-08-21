c3e9360087b2914daf9efd665ed8790ed119ec1c
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-08-21 21:16:06 +0530

    Time: 101 ms (50.08%), Space: 45.1 MB (63.86%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
+    bool canCross(vector<int>& stones) {
+        int n = stones.size();
+        unordered_map<int, unordered_set<int>> mp;
 
-    map<pair<int,int>,bool> dp;
-
-    bool solve(vector<int>& stones,int index,int lastJump){
-
-        if(index==stones.size()-1)
-            return true;
-
-        if(dp.count({index,lastJump}))
-            return dp[{index,lastJump}];
-
-        for(int next=index+1;next<stones.size();next++){
-
-            int jump=stones[next]-stones[index];
-
-            if(jump<lastJump-1)
-                continue;
-
-            if(jump>lastJump+1)
-                break;
-
-            if(solve(stones,next,jump))
-                return dp[{index,lastJump}]=true;
+        for (int stone : stones) {
+            mp[stone];
         }
+        // first 0 jump
+        mp[0].insert(0);
+        for (int i = 0; i < n; i++) {
+            int pos = stones[i];
+            // iske liye jitne possible try
+            for (int jump : mp[pos]) {
 
-        return dp[{index,lastJump}]=false;
-    }
-
-    bool canCross(vector<int>& stones){
-
-        return solve(stones,0,0);
-
+                for (int nj = jump - 1; nj <= jump + 1; nj++) {
+                    if (nj <= 0) {
+                        continue;
+                    }
+                    // next pos calc
+                    int nextpos = pos + nj;
+                    // agar last h toh true
+                    if (nextpos == stones[n - 1]) {
+                        return true;
+                    }
+                    // check stone h ki nhi
+                    if (mp.count(nextpos)) {
+                        mp[nextpos].insert(nj);
+                    }
+                }
+            }
+        }
+        return false;
     }
 };
