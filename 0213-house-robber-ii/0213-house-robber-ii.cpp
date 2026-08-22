f66407b9ce651e440559dcc85ddd74ca24f1fa99
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-08-22 15:15:15 +0530

    Time: 0 ms (100%), Space: 10 MB (97.33%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    // end is the last idx we consider
-    int solve(int i, vector<int>& nums, int end, vector<int>& dp) {
-        if (i > end) {
-            return 0;
-        }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        int take = nums[i] + solve(i + 2, nums, end, dp);
-        int nottake = solve(i + 1, nums, end, dp);
+    int solve(int start,vector<int>& nums,int end){
+        //ans from prev house
+        int prev1=0;
+        //ans from two house before
+        int prev2=0;
+        for(int i=start;i<=end;i++){
+            int take=nums[i]+prev2;
+            int nottake=prev1;
 
-        return dp[i] = max(take, nottake);
+            int curr=max(take,nottake);
+
+            //prev ko forward
+            prev2=prev1;
+            prev1=curr;
+            
+        }
+        return prev1;
     }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp1(n, -1);
-        vector<int> dp2(n, -1);
-        if (n == 1) {
+        int n=nums.size();
+        if(n==1){
             return nums[0];
         }
-        // first to second last
-        int case1 = solve(0, nums, n - 2, dp1);
-        // second to last
-        int case2 = solve(1, nums, n - 1, dp2);
-
-        return max(case1, case2);
+        int case1=solve(0,nums,n-2);
+        int case2=solve(1,nums,n-1);
+        
+        return max(case1,case2);
     }
 };
