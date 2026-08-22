c38538ffa78517495c85267f4bae8778360f8868
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-08-22 20:55:29 +0530

    Time: 0 ms (100%), Space: 15.8 MB (75.25%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int solve(int i, vector<int>& points, vector<int>& dp) {
-        if (i < 0) {
-            return 0;
-        }
-        if (i == 0) {
-            return points[0];
-        }
-        if(dp[i] != -1)
-            return dp[i];
-
-        int take = points[i] + solve(i - 2, points, dp);
-        int nottake = solve(i - 1, points, dp);
-
-        return dp[i]=max(take, nottake);
-    }
     int deleteAndEarn(vector<int>& nums) {
-
         int maxi = *max_element(nums.begin(), nums.end());
         vector<int> points(maxi + 1, 0);
         for (int x : nums) {
             points[x] += x;
         }
-        vector<int> dp(maxi + 1, -1);
-        return solve(maxi, points, dp);
+        vector<int> dp(maxi + 1, 0);
+        dp[0] = 0;
+        if (maxi >= 1) {
+            dp[1] = points[1];
+        }
+        for (int i = 2; i <= maxi; i++) {
+            int take = points[i] + dp[i - 2];
+            int nottake = dp[i - 1];
+
+            dp[i] = max(take, nottake);
+        }
+        return dp[maxi];
     }
 };
