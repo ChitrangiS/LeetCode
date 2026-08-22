87e79e28c469a727d23410d3cff98f6ab26fd004
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-08-22 15:55:02 +0530

    Time: 0 ms (100%), Space: 16.1 MB (64.36%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int solve(int day, int buy, vector<int>& prices, vector<vector<int>>& dp) {
-        if (day >= prices.size()) {
-            return 0;
-        }
-        if (dp[day][buy] != -1) {
-            return dp[day][buy];
-        }
-        while (buy == 1) {
-            int take = -prices[day] + solve(day + 1, 0, prices, dp);
-            int nottake = solve(day + 1, 1, prices, dp);
-
-            return dp[day][buy] = max(take, nottake);
-        }
-        int sell = prices[day] + solve(day + 2, 1, prices, dp);
-        int notsell = solve(day + 1, 0, prices, dp);
-
-        return dp[day][buy] = max(sell, notsell);
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return solve(0, 1, prices, dp);
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+        for (int day = n - 1; day >= 0; day--) {
+            int buy = max(-prices[day] + dp[day + 1][0], dp[day + 1][1]);
+            int sell = max(prices[day] + dp[day + 2][1], dp[day + 1][0]);
+
+            dp[day][1] = buy;
+            dp[day][0] = sell;
+        }
+        return dp[0][1];
     }
 };
