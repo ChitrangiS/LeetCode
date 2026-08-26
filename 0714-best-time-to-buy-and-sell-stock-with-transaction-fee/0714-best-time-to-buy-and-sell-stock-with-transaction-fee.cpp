0c063178b17f378c1a13286ad11413a182d3e7f1
Chitrangi Samal <165680976+ChitrangiS@users.noreply.github.com>
2026-08-26 17:05:33 +0530

    Time: 110 ms (41.9%), Space: 103.1 MB (60.66%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int solve(int i, int buy, int fee, vector<int>& prices,
-              vector<vector<int>>& dp) {
-        if (i == prices.size()) {
-            return 0;
-        }
-        if (dp[i][buy] != -1) {
-            return dp[i][buy];
-        }
-        if (buy == 1) {
-            int notbuy = solve(i + 1, 1, fee, prices, dp);
-            int buystock = -prices[i] + solve(i + 1, 0, fee, prices, dp);
-            return dp[i][buy] = max(notbuy, buystock);
-        } else {
-            int notsell = solve(i + 1, 0, fee, prices, dp);
-            int sellstock = prices[i] - fee + solve(i + 1, 1, fee, prices, dp);
-            return dp[i][buy] = max(notsell, sellstock);
-        }
-    }
     int maxProfit(vector<int>& prices, int fee) {
+
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return solve(0, 1, fee, prices, dp);
+
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            
+            int notBuy = dp[i + 1][1];
+
+            int buyStock =
+                -prices[i] + dp[i + 1][0];
+
+            dp[i][1] = max(notBuy, buyStock);
+
+            int notSell = dp[i + 1][0];
+
+            int sellStock =
+                prices[i] - fee + dp[i + 1][1];
+
+            dp[i][0] = max(notSell, sellStock);
+        }
+
+        return dp[0][1];
     }
 };
